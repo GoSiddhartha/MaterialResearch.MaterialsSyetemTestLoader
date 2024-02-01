a50dd8ab67fb66867172a23bd37bed0d89920c1b
dkSidNan <siddhartha.nandy@LEGO.com>
2024-02-01 14:49:00 +0100

    deploy via giyhub push

--- MaterialResearch.MaterialsSyetemTestLoader/Program.cs
+++ MaterialResearch.MaterialsSyetemTestLoader/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables();
+    })
     .Build();
 
 host.Run();
--- MaterialResearch.MaterialsSyetemTestLoader/TestFileLoader.cs
+++ MaterialResearch.MaterialsSyetemTestLoader/TestFileLoader.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MaterialResearch.MaterialsSyetemTestLoader;
@@ -9,26 +10,29 @@ namespace MaterialResearch.MaterialsSyetemTestLoader;
 public class TestFileLoader
 {
     private readonly ILogger _logger;
+    private readonly IConfiguration _configuration;
 
-    public TestFileLoader(ILoggerFactory loggerFactory)
+    public TestFileLoader(ILoggerFactory loggerFactory, IConfiguration configuration)
     {
         _logger = loggerFactory.CreateLogger<TestFileLoader>();
+        _configuration = configuration;
     }
 
     [Function("TestFileLoader")]
     public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-
         var response = req.CreateResponse(HttpStatusCode.OK);
+       
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        StreamReader reader = new StreamReader( req.Body );
-        string requestText = reader.ReadToEnd();
-        response.WriteString("Hello World! ");
-
-        response.WriteString("I did it, Vibeke!");
-
+    
+        var testConfig = _configuration["secret"];
+        if(string.IsNullOrEmpty(testConfig))
+            response.WriteString("The secret is empty");
+        else
+            response.WriteString(testConfig);
+        
         return response;
     }
 }
